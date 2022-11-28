1fa12befb8c60e1588f4ea794c6bd8cf55e2e8dd
Araz Ashkan Abedi <arazabedi@hotmail.com>
2022-11-28 17:53:16 +0000

    projects/users controller end day 1

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,30 +1,25 @@
 class ProjectsController < ApplicationController
-  def list
+  def index
     @projects = Project.all
   end
-
   def show
     @project = Project.find(params[:id])
   end
-
   def new
     @project = Project.new
   end
-
   def create
     @project = Project.new(project_params)
+    @project.user = current_user
     if @project.save
-      redirect_to :action => 'list'
+      redirect_to project_path(@project)
     else
-      @projects = Project.all
-      render :action => 'new'
+      render :new
     end
   end
-
   def edit
     @project = Project.find(params[:id])
   end
-
   def update
     @project = Project.find(params[:id])
     if @project.update_attributes(project_param)
@@ -34,19 +29,25 @@ class ProjectsController < ApplicationController
       render :action => 'edit'
     end
   end
-
   def delete
-    Project.find(params[:id]).destroy
-    redirect_to :action => 'list'
+    if @project.destroy
+      redirect_to projects_path(@project)
+    else
+      render :index
   end
+end
 
-  private
+private
 
-  def project_params
-    params.require(:projects).permit(:owner_id, :name, :description)
-  end
+def set_project
+  @project = Project.find(params[:id])
+end
 
-  def project_param
-    params.require(:project).permit(:owner_id, :name, :description)
-  end
+def project_params
+  params.require(:projects).permit(:owner_id, :name, :description)
+end
+
+def project_param
+  params.require(:project).permit(:owner_id, :name, :description)
+end
 end
