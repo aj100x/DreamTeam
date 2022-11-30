e85c3c05bad3c34d2c86bcf423c5912abec9157f
Alice Teilhard <alice.teilhard@gmail.com>
2022-11-30 15:43:22 +0000

    added the confirmation altert

--- app/controllers/collaboration_requests_controller.rb
+++ app/controllers/collaboration_requests_controller.rb
@@ -5,8 +5,12 @@ class CollaborationRequestsController < ApplicationController
     @collaboration_request = CollaborationRequest.new(collaboration_request_params)
     @collaboration_request.requester = current_user
     @collaboration_request.project = @project
-    @collaboration_request.save!
-    redirect_to project_path(@project)
+    if @collaboration_request.save
+      redirect_to project_path(@project)
+      flash[:notice] = "Request Sent"
+    else
+      render :new, status: :unprocessable_entity
+  end
   end
 
   def new
