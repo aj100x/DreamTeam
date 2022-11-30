5f2dc4fd13bd5895dbd16aaff769d1fa47755427
Araz Ashkan Abedi <arazabedi@hotmail.com>
2022-11-30 16:21:13 +0000

    youtube embed through new project form and carousel added to project show

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -15,7 +15,7 @@ class ProjectsController < ApplicationController
   def create
     @project = Project.new(project_params)
     @project.owner = current_user
-    if @project.save
+    if @project.save!
       redirect_to project_path(@project)
       flash[:notice] = "New project succesfully created."
     else
@@ -58,6 +58,6 @@ class ProjectsController < ApplicationController
 
 
   def project_params
-    params.require(:project).permit(:owner_id, :name, :description, :photo)
+    params.require(:project).permit(:owner_id, :name, :description, :yturl, photos: [])
   end
 end
