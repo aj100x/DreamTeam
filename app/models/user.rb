edc8e1a33315565d00c54b66c7a1284c5122307d
aj100x <arsalanjamil64@gmail.com>
2022-12-02 12:52:31 +0000

    DropDownUpdate

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   has_many :collaboration_requests_as_requester, foreign_key: "requester_id", class_name: "CollaborationRequest"
   has_many :collaboration_requests_on_projects_owned, through: :owned_projects, source: :collaboration_requests
   has_many :collaboration_requests, foreign_key: "requester_id"
-  has_one_attached :image
+  has_one_attached :photo
   validates :first_name, :last_name, :bio, :tagline, presence: true, on: :update
   # has_many :colloboration_request_demands, foreign_key: "requestee_id", class_name: "CollaborationRequest"
 
