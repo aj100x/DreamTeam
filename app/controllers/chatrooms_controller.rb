4c282afe95912ca6bffe8cb5015e222dc162c3c2
Sloane Carroll <sloanecarroll15@gmail.com>
2022-12-05 16:21:12 +0000

    chatroom function

---
+++ app/controllers/chatrooms_controller.rb
@@ -0,0 +1,31 @@
+class ChatroomsController < ApplicationController
+
+  def index
+    @chatrooms = current_user.chatrooms
+    @messages = Message.all
+  end
+
+  def show
+    @chatroom = Chatroom.find(params[:id])
+    @message = Message.new
+    @chatrooms = Chatroom.all
+  end
+
+  # def create
+  #   # @chatroom = Chatroom.new(chatroom_params)
+  #   raise
+  #   Chatroom.create!(project: )
+  #   @chatroom.requester = current_user
+  #   if @chatroom.save
+  #     redirect_to chatroom_path(@chatroom)
+  #   else
+  #     render "users/show"
+  #   end
+  # end
+
+  private
+
+  def chatroom_params
+    params.require(:chatroom).permit(:receiver_id)
+  end
+end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,9 +1,11 @@
 class Project < ApplicationRecord
   has_many_attached :photos
   belongs_to :owner, class_name: 'User', foreign_key: 'owner_id'
-  has_many :project_users, dependent: :destroy  
+  has_many :project_users, dependent: :destroy
   has_many :collaborators, through: :project_users, source: :user
   has_many :collaboration_requests, dependent: :destroy
+  has_one :chatroom
   validates :name, :description, :yturl, presence: true
   validates :description, length: { minimum: 25 }
 end
+ 
