2c552aefd7720a586815d58ae1d0f78b4fce3fff
Alice Teilhard <alice.teilhard@gmail.com>
2022-12-05 22:47:55 +0000

    display new skills in the profile show

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,19 +10,16 @@ class User < ApplicationRecord
   has_many :collaboration_requests_on_projects_owned, through: :owned_projects, source: :collaboration_requests
   has_many :collaboration_requests, foreign_key: "requester_id"
   has_one_attached :photo
-  validates :first_name, :last_name, :bio, :tagline, presence: true, on: :update
+  validates :first_name, :last_name, :bio, :skill, :tagline, presence: true, on: :update
   # has_many :colloboration_request_demands, foreign_key: "requestee_id", class_name: "CollaborationRequest"
 
   def full_name
     "#{first_name.capitalize} #{last_name.capitalize}"
   end
 
+
   def education_details
     return unless course && university
     "Studied #{course.capitalize} at #{university.capitalize} from #{uni_start_year} to #{uni_end_year}"
   end
 end
-
-
-## set up accept/decline in collabreq controller
-# method to accept.decline in collabl c
