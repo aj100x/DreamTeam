b7b1749bd63f4e3db63b8d2ffea699d3d2d0feb0
Alice Teilhard <alice.teilhard@gmail.com>
2022-12-05 21:49:27 +0000

    skill added to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+          :recoverable, :rememberable, :validatable
   has_many :project_users, dependent: :destroy
   has_many :projects, through: :project_users
   has_many :owned_projects, foreign_key: "owner_id", class_name: "Project"
